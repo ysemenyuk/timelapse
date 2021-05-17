7138a426bf541e0ec874cd7ca48825d86cf7c33d
ysemenyuk <invent@list.ru>
2021-05-17 03:17:01 +0300

    del local

--- local/bin/startGID.js
+++
@@ -1,5 +0,0 @@
-import getImagesDay from '../funcs/getImagesDay.js';
-
-import { cam1 } from '../cameras.js';
-
-getImagesDay(cam1);
--- local/bin/startGIDfromRTSP.js
+++
@@ -1,6 +0,0 @@
-import getImagesDayRTSP from '../funcs/getImagesDayRTSP.js';
-
-import { cam2 } from '../cameras.js';
-
-getImagesDayRTSP(cam2);
-// getImagesDayRTSP(cam3)
